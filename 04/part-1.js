ccddaeb9fa4d494db04e5a1b975f0fcc365208f1
Spencer Fitzgerald <spencerfitz0196@gmail.com>
2022-12-03 23:23:57 -0700

    Complete day 4

---
+++ 04/part-1.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function pairOverlaps(pairA, pairB) {
+  if (pairA[0] <= pairB[0] && pairA[1] >= pairB[1]) {
+    return true
+  } else if (pairB[0] <= pairA[0] && pairB[1] >= pairA[1]) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function main() {
+  let data = fs.readFileSync('./input.txt', 'utf-8').split('\n')
+  let totalCompleteOverlaps = 0
+
+  data.forEach(element => {
+    let [pairA, pairB] = element.split(',')
+    pairA = pairA.split('-').map(el => Number(el))
+    pairB = pairB.split('-').map(el => Number(el))
+
+    if (pairOverlaps(pairA, pairB)) {
+      totalCompleteOverlaps++
+    }
+  });
+  console.log(totalCompleteOverlaps)
+}
+
+main()
